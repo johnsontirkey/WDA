b4173122c1a3cda0acd7a46f3e56561f1e8d2fe0
jdgrath20 <johnson.tirkey2020@gmail.com>
2022-10-28 01:30:16 +0530

    first commit

---
+++ js/dropdown.js
@@ -0,0 +1,17 @@
+const dropdown_bca = document.querySelector('.dropdown_bca');
+
+dropdown_bca.addEventListener('click', ()=> {
+    dropdown_bca.classList.toggle('active');
+});
+
+const dropdown_mca = document.querySelector('.dropdown_mca');
+
+dropdown_mca.addEventListener('click', ()=> {
+    dropdown_mca.classList.toggle('active');
+});
+
+const dropdown_phd = document.querySelector('.dropdown_phd');
+
+dropdown_phd.addEventListener('click', ()=> {
+    dropdown_phd.classList.toggle('active');
+});
